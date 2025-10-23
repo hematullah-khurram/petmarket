b62730c954a59456ed9f57eaef69b8a037e133e8
hematullah <hematullah.khurram@gmail.com>
2025-10-23 21:06:04 +0200

    code debugging

--- market/urls.py
+++ market/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    # path('', views.home, name='home'),
     path('pet/<int:pk>/', views.pet_detail, name='pet_detail'),
     path('pet/add/', views.add_pet, name='pet_create'),   # Add pet
     path('pet/<int:pk>/edit/', views.edit_pet, name='pet_edit'),  # Edit pet
     path('pet/<int:pk>/delete/', views.delete_pet, name='pet_delete'),  # Delete pet
     path('signup/', views.signup_view, name='signup'),
-    # path('', views.pet_list, name='pet_list'),
+    path('', views.pet_list, name='pet_list'),
 ]
