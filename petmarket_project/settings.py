088a4a45cd470ba3dceaf539fa242a1da4831518
hematullah <hematullah.khurram@gmail.com>
2025-10-24 13:39:09 +0200

    Fix media display on Heroku

--- petmarket_project/settings.py
+++ petmarket_project/settings.py
@@ -141,9 +141,27 @@ USE_TZ = True
 # -------------------------------------------------------------------
 # Static and Media files
 # -------------------------------------------------------------------
+# Static files
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # for collectstatic/deployment
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# WhiteNoise (for static)
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # keep this
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
 
 
 
