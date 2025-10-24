2ecaac5dbd22b7dd0513a93cfe23d660025862ef
hematullah <hematullah.khurram@gmail.com>
2025-10-24 14:00:18 +0200

    added edit button

--- petmarket_project/settings.py
+++ petmarket_project/settings.py
@@ -141,27 +141,9 @@ USE_TZ = True
 # -------------------------------------------------------------------
 # Static and Media files
 # -------------------------------------------------------------------
-# Static files
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 
-# Media files
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
-# WhiteNoise (for static)
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # keep this
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # for collectstatic/deployment
 
 
 
